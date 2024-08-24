1fc89c23dc272cfac4cb27cc6d5f947be0926617
bassimahmed <bassimahmed20@gmail.com>
2024-08-24 15:15:01 +0300

    final

---
+++ assignment 1 entity frame work/Entities/Course.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment_1_entity_frame_work.Entities
+{
+    internal class Course
+    {
+        public int ID { get; set; }
+        public int Duration { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Top_ID { get; set; }
+    }
+
+}
---
+++ assignment 1 entity frame work/Entities/Topic.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment_1_entity_frame_work.Entities
+{
+    internal class Topic
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+
+}
